4a8576a5466f51ff80ef5350cb60a7278c760fff
yinyue200 <yinyue200@outlook.com>
2017-12-22 22:05:31 +0800

    first commit

--- ThreadHelper/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ThreadHelper
-{
-    public class Class1
-    {
-    }
-}
---
+++ ThreadHelper/ThreadHelper.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.Threading;
+
+namespace Yinyue200.Threading.UI
+{
+    public abstract class ThreadHelper
+    {
+        public static JoinableTaskContext JoinableTaskContext { get; set; }
+
+        public static JoinableTaskFactory JoinableTaskFactory => JoinableTaskContext.Factory;
+    }
+}
